84a505c43e154a30838d2f4023555aecce5591b6
vagish-krishna <vagishkrishnarameshkannan04@gmail.com>
2026-09-24 12:01:00 +0530

    Add filtering operations for flights dataset

---
+++ flights dataset.R
@@ -0,0 +1,7 @@
+i)filter(flights, arr_delay>=120)
+
+ii) filter(flights, dest=="IAH “|dest=="HOU")
+
+iii) filter (flights, carrier=="AA"| carrier=="DL"| carrier =="UA")
+
+iv) filter(flights, month>=7,month<=9)
